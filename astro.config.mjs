91cab34e7b2ac4841bc15ef43f00be55c498e07e
eFFx <chanoo104@naver.com>
2026-01-12 13:32:45 +0900

    feat: wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,31 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import rehypeExternalLinks from 'rehype-external-links'
-import rehypePresetMinify from 'rehype-preset-minify'
-import vercelStatic from '@astrojs/vercel/static'
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypePresetMinify from "rehype-preset-minify";
 
-const rehypeExternalLinksConfig = [
-	rehypeExternalLinks,
-	{ target: '_blank', rel: ['noopener', 'noreferrer'] }
-]
+const rehypeExternalLinksConfig = [rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] }];
 
 export default defineConfig({
-	site: 'https://cpu.land/',
-	trailingSlash: 'never',
-	output: 'static',
-	adapter: vercelStatic(),
-	server: {
-		port: parseInt(process.env.PORT || '3000')
-	},
-	integrations: [
-		mdx({
-			rehypePlugins: [ rehypeExternalLinksConfig, rehypePresetMinify ]
-		}),
-		sitemap({
-			filter: page => page !== 'https://cpu.land/404'
-		})
-	],
-	markdown: {
-		smartypants: true,
-		rehypePlugins: [ rehypeExternalLinksConfig ],
-		shikiConfig: {
-			theme: 'one-dark-pro'
-		}
-	}
-})
+  site: "https://cpu.land/",
+  trailingSlash: "never",
+  output: "static",
+  server: {
+    port: parseInt(process.env.PORT || "3000"),
+  },
+  integrations: [
+    mdx({
+      rehypePlugins: [rehypeExternalLinksConfig, rehypePresetMinify],
+    }),
+    sitemap({
+      filter: (page) => page !== "https://cpu.land/404",
+    }),
+  ],
+  markdown: {
+    smartypants: true,
+    rehypePlugins: [rehypeExternalLinksConfig],
+    shikiConfig: {
+      theme: "one-dark-pro",
+    },
+  },
+});
